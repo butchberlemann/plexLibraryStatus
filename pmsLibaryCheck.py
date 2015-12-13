1361cb482678b5fe6bd857ebaf5a2541443d6945
butch berlemann <butchberlemann@gmail.com>
2015-12-12 21:26:47 -0700

    Refinements :)

--- pmsLibaryCheck.py
+++ pmsLibaryCheck.py
@@ -1,21 +1,39 @@
 import urllib2 as URL
 import xml.etree.ElementTree as ET
+import json
+purgeDir = " E:\Purge"
 
 content = URL.urlopen("http://127.0.0.1:32400/library/all").read()
 root = ET.fromstring(content)
 child = root.find("Video")
 csvOutput = open('./testfile.csv','w')
 
-csvOutput.write("Title, rating \r")
+csvOutput.write("Title, Rotten Tomato Rating, IMDB, Metascore, File loc \r")
 
 for child in root:
-    csvOutput.write(child.attrib.get('title') + ",")
+    title = child.attrib.get('title')
+    media = child.find("Media").find("Part")
+    
+    csvOutput.write(title.replace(',','') + ",")
     audienceRatingImage = child.attrib.get('audienceRatingImage')
+
     if audienceRatingImage != None:
-        if audienceRatingImage.
-        csvOutput.write(rating + ",\r")
+        csvOutput.write(audienceRatingImage.replace("rottentomatoes://image.rating.", "") + "," )
     else:
-        csvOutput.write("No Rating Found ,\r")
+        csvOutput.write("No Rating Found , ")
+    #Open Movie DB
+    openMovie = URL.urlopen("http://www.omdbapi.com/?i=&t=" + title.replace(' ', '+') ).read()
+    parsed_json = json.loads(openMovie)
+
+    if parsed_json['Response'] == "False":
+        csvOutput.write(",")
+        csvOutput.write(",")
+    else:
+        csvOutput.write(parsed_json['imdbRating'] + ",")
+        csvOutput.write(parsed_json['Metascore'] + ",")
+
+    csvOutput.write("Move-Item " + media.attrib.get("file")+ purgeDir + ",\r")
     #break
+    
 
 csvOutput.close()    
